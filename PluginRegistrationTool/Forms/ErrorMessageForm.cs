c9378c737b23df0db7f4e5fb2264444bee3cf4ba
True Macacq <macacq@gmail.com>
2015-03-27 15:10:58 +0100

    Removing unused usings

--- PluginRegistrationTool/Forms/ErrorMessageForm.cs
+++ PluginRegistrationTool/Forms/ErrorMessageForm.cs
@@ -18,16 +18,11 @@
 namespace PluginRegistrationTool.Forms
 {
     using System;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.Data;
-    using System.Drawing;
     using System.Globalization;
     using System.IO;
     using System.Runtime.Serialization;
     using System.ServiceModel;
     using System.Text;
-    using System.Web.Services.Protocols;
     using System.Windows.Forms;
     using System.Xml;
     using Microsoft.Xrm.Sdk;
