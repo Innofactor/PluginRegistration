ff3d97635e9e315e5094e9311006e6fcf518f839
Trou Macacq <macacq@gmail.com>
2016-08-10 13:19:05 +0200

    Adding test for attribute of type DateTime #1

--- Xrm.Sdk.PluginRegistration.Tests/Enities/SdkMessageFixture.cs
+++ Xrm.Sdk.PluginRegistration.Tests/Enities/SdkMessageFixture.cs
@@ -4,6 +4,7 @@
     using Entities.Transformation;
     using Microsoft.Xrm.Sdk;
     using NUnit.Framework;
+    using System;
 
     [TestFixture]
     class SdkMessageFixture
@@ -33,6 +34,20 @@
 
             // Assert
             Assert.That(earlyBound.AutoTransact == (bool?)lateBound.Attributes["autotransact"]);
+        }
+
+        [Test]
+        public void CheckDateTimeAttribute()
+        {
+            // Arrange
+            var lateBound = new Entity("sdkmessage");
+            lateBound.Attributes.Add("createdon", DateTime.Today);
+
+            // Act
+            var earlyBound = Magic.Cast<SdkMessage>(lateBound);
+
+            // Assert
+            Assert.That(earlyBound.CreatedOn.Value.Day == ((DateTime?)lateBound.Attributes["createdon"]).Value.Day);
         }
     }
 }
