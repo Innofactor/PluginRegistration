7b7d94eb6eac3c6650c3a4577ff254882132b2a7
Trou Macacq <macacq@gmail.com>
2016-08-10 11:15:26 +0200

    Adding first empty test fixture #1

---
+++ Xrm.Sdk.PluginRegistration.Tests/Enities/SdkMessageFixture.cs
@@ -0,0 +1,14 @@
+﻿namespace Xrm.Sdk.PluginRegistration.Tests.Enities
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    class SdkMessageFixture
+    {
+    }
+}
