a6c774d08f071293900802c154afa14f528fabfa
Trou Macacq <macacq@gmail.com>
2016-08-10 13:20:27 +0200

    Simplifying magic cast code #1

--- Xrm.Sdk.PluginRegistration/Entities/Transformation/Magic.cs
+++ Xrm.Sdk.PluginRegistration/Entities/Transformation/Magic.cs
@@ -2,30 +2,19 @@
 {
     using Microsoft.Xrm.Sdk;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public static class Magic
     {
         public static T Cast<T>(Entity entity)
             where T: Entity
         {
-            var type = typeof(T);
-
             var instance = (T)Activator.CreateInstance(typeof(T));
-            
-            foreach (var property in type.GetProperties().Where(x => x.IsDefined(typeof(AttributeLogicalNameAttribute), false)))
+
+            if (instance != null)
             {
-                var attributeName = ((AttributeLogicalNameAttribute)property.GetCustomAttributes(false).FirstOrDefault()).LogicalName;
-
-                if (entity.Attributes.ContainsKey(attributeName))
-                {
-                    property.SetValue(instance, entity[attributeName]);
-                }
+                instance.Attributes = entity.Attributes;
             }
-
+            
             return instance;
         }
     }
