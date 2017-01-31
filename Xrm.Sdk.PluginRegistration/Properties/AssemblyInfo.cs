fc6baf4b283429a768e88fe2ac27efd4d40d2b22
Alexey Shytikov <alexey.shytikov@innofactor.com>
2017-01-31 16:08:19 +0100

    Pumping version to 3.1.4.2

--- Xrm.Sdk.PluginRegistration/Properties/AssemblyInfo.cs
+++ Xrm.Sdk.PluginRegistration/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.1.4.1")]
-[assembly: AssemblyFileVersion("3.1.4.1")]
+[assembly: AssemblyVersion("3.1.4.2")]
+[assembly: AssemblyFileVersion("3.1.4.2")]
