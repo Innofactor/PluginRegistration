a46ae5337866047ffeee5a8b036d62ebf337a79b
Imran Akram <imran.akram@cinteros.se>
2018-01-04 15:19:34 +0100

    Added PluginType field to CsvModel and improved formatting

--- Xrm.Sdk.PluginRegistration/Models/CsvModel.cs
+++ Xrm.Sdk.PluginRegistration/Models/CsvModel.cs
@@ -4,31 +4,43 @@
 
     public class CsvModel
     {
+        [Name("Plugin Type")]
+        public string PluginType { get; set; }
+
         [Name("Assembly")]
         public string AssemblyName { get; set; }
-        [Name("Plugin/Workflow")]
+
+        [Name("Plugin/Workflow Name")]
         public string TypeName { get; set; }
+
         [Name("Description")]
         public string Description { get; set; }
+
         [Name("Primary Entity")]
         public string PrimaryEntity { get; set; }
+
         [Name("Secondary Entity")]
         public string SecondaryEntity { get; set; }
+
         [Name("Message")]
         public string Message { get; set; }
+
         [Name("Execution Mode")]
         public string ExecutionMode { get; set; }
+
         [Name("Stage")]
         public string Stage { get; set; }
+
         [Name("Execution Order")]
         public string ExecutionOrder { get; set; }
+
         [Name("Deployment")]
         /// <summary>
         /// ServerOnly, Offline, Both
         /// </summary>
         public string Deployment { get; set; }
+
         [Name("Filtering Attributes ")]
         public string FilteringAttributes { get; set; }
-       
     }
 }
