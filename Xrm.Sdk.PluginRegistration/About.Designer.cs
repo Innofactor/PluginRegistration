d95bb19e78fbe06b2ced712cd75024dddf8d35f9
Imran Akram <imran.akram@cinteros.se>
2018-12-20 10:35:59 +0100

    Adding About dialog

---
+++ Xrm.Sdk.PluginRegistration/About.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Xrm.Sdk.PluginRegistration
+{
+    partial class About
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listAssemblies = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listAssemblies);
+            this.groupBox1.Location = new System.Drawing.Point(34, 169);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(667, 300);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            // 
+            // listAssemblies
+            // 
+            this.listAssemblies.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listAssemblies.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listAssemblies.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listAssemblies.FullRowSelect = true;
+            this.listAssemblies.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listAssemblies.Location = new System.Drawing.Point(3, 18);
+            this.listAssemblies.Name = "listAssemblies";
+            this.listAssemblies.ShowGroups = false;
+            this.listAssemblies.Size = new System.Drawing.Size(661, 279);
+            this.listAssemblies.TabIndex = 0;
+            this.listAssemblies.UseCompatibleStateImageBehavior = false;
+            this.listAssemblies.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Assembly";
+            this.columnHeader1.Width = 364;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Version";
+            this.columnHeader2.Width = 224;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Xrm.Sdk.PluginRegistration.Resources.dynamics365_icon_120;
+            this.pictureBox1.Location = new System.Drawing.Point(34, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.Location = new System.Drawing.Point(177, 25);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(404, 39);
+            this.lblHeading.TabIndex = 19;
+            this.lblHeading.Text = "Plugin Registration Tool";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.Location = new System.Drawing.Point(184, 79);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(500, 46);
+            this.lblDescription.TabIndex = 20;
+            this.lblDescription.Text = "This is special flavor of classical Plugin Registration Tool provided by Microsof" +
+    "t as CRM SDK code sample.";
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 498);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblHeading);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "About";
+            this.Text = "About";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listAssemblies;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Label lblDescription;
+    }
+}
---
+++ Xrm.Sdk.PluginRegistration/About.cs
@@ -0,0 +1,89 @@
+﻿namespace Xrm.Sdk.PluginRegistration
+{
+    using CsvHelper;
+    using OfficeOpenXml;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Windows.Forms;
+
+    public partial class About : Form
+    {
+        #region Public Constructors
+
+        public About()
+        {
+            InitializeComponent();
+            DummyUse();
+            PopulateAssemblies();
+        }
+
+        private void DummyUse()
+        {
+            //var fileInfo = new FileInfo("test.txt");
+            //using (var csv = new CsvWriter(fileInfo)) ;
+            //{
+
+            //}
+            //using (var xlPackage = new ExcelPackage())
+            //{
+
+            //}
+        }
+        #endregion Public Constructors
+
+        #region Private Methods
+
+        private static string AssemblyPrioritizer(string assemblyName)
+        {
+            return
+                assemblyName.Equals("XrmToolBox") ? "AAAAAAAAAAAA" :
+                assemblyName.Contains("XrmToolBox") ? "AAAAAAAAAAAB" :
+                assemblyName.Equals(Assembly.GetExecutingAssembly().GetName().Name) ? "AAAAAAAAAAAC" :
+                //assemblyName.Contains("Jonas") ? "AAAAAAAAAAAD" :
+                //assemblyName.Contains("Rappen") ? "AAAAAAAAAAAE" :
+                assemblyName.Contains("Innofactor") ? "AAAAAAAAAAAF" :
+                assemblyName.Contains("Cinteros") ? "AAAAAAAAAAAG" :
+                assemblyName;
+        }
+
+        private ListViewItem GetListItem(AssemblyName a)
+        {
+            var item = new ListViewItem(a.Name);
+            item.SubItems.Add(a.Version.ToString());
+            return item;
+        }
+
+        private List<AssemblyName> GetReferencedAssemblies()
+        {
+            
+            var names = Assembly.GetExecutingAssembly().GetReferencedAssemblies()
+                    .Where(a => !a.Name.Equals("mscorlib") && !a.Name.StartsWith("System") && !a.Name.Contains("CSharp")).ToList();
+            names.Add(Assembly.GetEntryAssembly().GetName());
+            names.Add(Assembly.GetExecutingAssembly().GetName());
+            names.Add(Assembly.LoadFrom("CsvHelper.dll")?.GetName());
+            names.Add(Assembly.LoadFrom("EPPlus.dll")?.GetName());
+            names = names.OrderBy(a => AssemblyPrioritizer(a.Name)).ToList();
+            return names;
+        }
+
+        private void PopulateAssemblies()
+        {
+            try
+            {
+                var assemblies = GetReferencedAssemblies();
+                var items = assemblies.Select(a => GetListItem(a)).ToArray();
+                listAssemblies.Items.Clear();
+                listAssemblies.Items.AddRange(items);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        #endregion Private Methods
+    }
+}
