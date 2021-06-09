b144331ef2935e87c39139788734e62f1e0179d5
Valdik <valdik5379455@gmail.com>
2021-06-09 11:06:09 +0200

    Results in Search window are anchored to top and resize with the dialog. (#99)
    Co-authored-by: Vladimir Mihal <v.mihal@avanade.com>

--- Xrm.Sdk.PluginRegistration/Forms/SearchForm.Designer.cs
+++ Xrm.Sdk.PluginRegistration/Forms/SearchForm.Designer.cs
@@ -68,7 +68,8 @@ namespace Xrm.Sdk.PluginRegistration.Forms
             // 
             // trvPlugins
             // 
-            this.trvPlugins.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.trvPlugins.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.trvPlugins.AutoExpand = false;
             this.trvPlugins.CrmTreeNodeSorter = null;
