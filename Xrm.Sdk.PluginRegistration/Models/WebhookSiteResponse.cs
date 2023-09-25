2ccaa818948e8a5134a86a3226f10935025465b6
Ivan Ficko <ificko@span.eu>
2023-09-25 12:39:51 +0200

    Webhook.site integration for testing webhooks (#119)
    * Added Webhook.site test URL generation button to webhook create dialog
    
    * Changed default webhook name to monitor URL
    
    * Added tooltip to generate webhook URL button

---
+++ Xrm.Sdk.PluginRegistration/Models/WebhookSiteResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace Xrm.Sdk.PluginRegistration.Models
+{
+    public class WebhookSiteResponse
+    {
+        public string uuid { get; set; }
+        public bool redirect { get; set; }
+        public object alias { get; set; }
+        public bool actions { get; set; }
+        public bool cors { get; set; }
+        public bool expiry { get; set; }
+        public int timeout { get; set; }
+        public bool premium { get; set; }
+        public object user_id { get; set; }
+        public bool password { get; set; }
+        public string ip { get; set; }
+        public string user_agent { get; set; }
+        public string default_content { get; set; }
+        public int default_status { get; set; }
+        public string default_content_type { get; set; }
+        public object premium_expires_at { get; set; }
+        public object description { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+    }
+}
